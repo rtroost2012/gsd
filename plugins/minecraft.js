284a79a13b8741052917115d2e3d3e7ca358719d
eoin <eoinperso@gmail.com>
2013-12-21 18:55:42 +0100

    Initial commit

---
+++ plugins/minecraft.js
@@ -0,0 +1,8 @@
+var settings = {};
+settings.stop_command = 'stop'
+settings.started_trigger = ')! For help, type "help" or "?"'
+settings.query = function (){};
+settings.maplist = function (){};
+settings.configlist = function (){};
+
+module.exports = settings;
