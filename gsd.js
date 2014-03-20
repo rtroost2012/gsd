b3dd7c937fa5f4c4c49f80c892a810f06e429750
eoin <eoinperso@gmail.com>
2014-03-20 10:44:29 +0100

    Add uri /gameservers/ that lists all gameservers configured on the daemon.

--- gsd.js
+++ gsd.js
@@ -38,7 +38,31 @@ restserver.use(
   }
 );
 
+restserver.get('/gameservers/', function info(req, res, next){
+  response = [];
+  servers.forEach(
+    function(gameserver){ 
+      response.push(gameserver.info());
+    });
+  res.send(response);
+});
+
 restserver.get('/gameservers/:id', function info(req, res, next){gameserver = servers[req.params.id]; res.send(gameserver.info());});
+restserver.put('/gameservers/:id', function info(req, res, next){
+  gameserver = servers[req.params.id];
+
+  config.servers[req.params.id].variables = req.params['variables']
+  console.log(config.servers[req.params.id])
+    
+  //config.servers[req.params.id].consoleport = req.params['consoleport']
+  //config.servers[req.params.id].overide_command_line = req.params['overide_command_line']
+  //config.servers[req.params.id].path = req.params['path']
+  //config.servers[req.params.id].gameport = req.params['gameport']
+  //config.servers[req.params.id].gamehost = req.params['gamehost']
+
+  res.send(gameserver.info());
+});
+
 restserver.get('/gameservers/:id/on', function on(req, res, next){gameserver = servers[req.params.id]; gameserver.turnon();res.send('ok')});
 restserver.get('/gameservers/:id/off', function off(req, res, next){gameserver = servers[req.params.id]; gameserver.turnoff();res.send('ok')});
 restserver.get('/gameservers/:id/restart', function restart(req, res, next){gameserver = servers[req.params.id]; gameserver.restart();res.send('ok')});
@@ -46,13 +70,13 @@ restserver.get('/gameservers/:id/configlist', function configlist(req, res, next
 restserver.get('/gameservers/:id/maplist', function maplist(req, res, next){gameserver = servers[req.params.id]; res.send(gameserver.maplist());});
 restserver.get('/gameservers/:id/query', function query(req, res, next){gameserver = servers[req.params.id]; res.send(gameserver.lastquery());});
 
-restserver.post('/gameservers/:id/console', function command(req, res, next){gameserver = servers[req.params.id]; gameserver.send(req.params.command); console.log("D");res.send('ok');});
+restserver.post('/gameservers/:id/console', function command(req, res, next){gameserver = servers[req.params.id]; gameserver.send(req.params.command); res.send('ok');});
 
 restserver.get('/gameservers/:id/addonsinstalled', function command(req, res, next){gameserver = servers[req.params.id]; res.send(gameserver.addonlist());});
 
 restserver.get(/^\/gameservers\/(\d+)\/file\/(.+)/, function(req, res, next) {gameserver = servers[req.params[0]];res.send({'contents':gameserver.readfile(req.params[1])});});
 restserver.put(/^\/gameservers\/(\d+)\/file\/(.+)/, function(req, res, next) {
-  if ('contents' in req.params){    
+  if ('contents' in req.params){
     gameserver = servers[req.params[0]];res.send(gameserver.writefile(req.params[1], req.params['contents']));
   }
   if ('url' in req.params){
@@ -61,7 +85,7 @@ restserver.put(/^\/gameservers\/(\d+)\/file\/(.+)/, function(req, res, next) {
 });
 // TODO : put send
 
-restserver.get('/gameservers/:id/gamemodes', function command(req, res, next){gameserver = servers[req.params.id]; gameserver.getgamemode(res);});
+restserver.get('/gameservers/:id/gamemodes', function command(req, res, next){gameserver = servers[req.params.id]; gameserver.getgamemodes(res);});
 restserver.put('/gameservers/:id/gamemodes', function command(req, res, next){gameserver = servers[req.params.id]; gameserver.installgamemode(req.params['gamemode']); res.send("ok");});
 restserver.del('/gameservers/:id/gamemodes', function command(req, res, next){gameserver = servers[req.params.id]; gameserver.getgamemode(res);});
 
